a032c28541d430c60815475c0166777edc14d29c
Ildar <krek666@yandex.ru>
2022-12-12 21:05:59 +0300

    Решили первую задачу

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача : Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+/*
+bool Pal(int number)
+{
+    int num = number;
+    string result = "";
+    while(num > 0)
+    {
+        int a = num % 10; 
+        num = num / 10;   
+        result = result + a;
+    }
+
+    int y = int.Parse(result);
+
+    if(y == number)
+    {
+        bool res = true;
+        return res;
+    }
+    else
+    {
+        bool res = false;
+        return res;
+    }
+}
+
+Console.Write("Input a number : ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+bool go = Pal(x);
+
+Console.WriteLine(go);
+*/
+
+// Задача : Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+
+
+
+
+
+
+
